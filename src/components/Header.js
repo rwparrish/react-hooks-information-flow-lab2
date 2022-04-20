32e56b646b54fd723e036a49a4d9dce00829f62d
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-04-20 10:36:01 -0600

    final

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+
+function Header({isDarkMode, onDarkModeClick}) {
+
+    return ( 
+        <div>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </div>
+    );
+}
+
+export default Header;
